b0d973c7de8260a19d3bfa649e3673614d606258
Jhoan Victor <jhoanvic.melo@gmail.com>
2021-01-06 16:31:21 -0300

    Insert, delete, update Product and Exceptions

--- src/main/java/com/coursejava/course/services/ProductService.java
+++ src/main/java/com/coursejava/course/services/ProductService.java
@@ -3,11 +3,18 @@ package com.coursejava.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.stereotype.Service;
 
 import com.coursejava.course.entities.Product;
 import com.coursejava.course.repositories.ProductRepository;
+import com.coursejava.course.services.exceptions.DatabaseException;
+import com.coursejava.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -21,7 +28,44 @@ public class ProductService {
 
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public Product insert(Product obj) {
+		try {
+			return repository.save(obj);
+		} catch (InvalidDataAccessApiUsageException e) {
+			throw new DatabaseException(e.getMessage());
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public Product update(Long id, Product obj) {
+		try {
+			Product entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Product entity, Product obj) {
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setPrice(obj.getPrice());
+		entity.setImgUrl(obj.getImgUrl());
 	}
 
 }
